427141c98eb3b3574b0d6bcbb1edc128e6b86027
post-rex <post-rex@pm.me>
2020-11-30 08:58:22 +0100

    forgot to commit this a week ago

--- src/pool/executor.rs
+++ src/pool/executor.rs
@@ -1,1 +1,20 @@
-pub struct ExecutionTimeWrapper
+use evc::OperationCache;
+use std::time::Instant;
+
+pub struct ExecutionTimeWrapper(Option<Instant>);
+
+enum ETOperation {
+	Set(Instant),
+	Clear,
+}
+
+impl evc::OperationCache for ExecutionTimeWrapper {
+	type Operation = ETOperation;
+
+	fn apply_operation(&mut self, operation: Self::Operation) {
+		self.0 = match operation {
+			ETOperation::Set(inst) => Some(inst),
+			ETOperation::Clear => None,
+		}
+	}
+}
--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -1,4 +1,6 @@
 use crate::task::Task;
+use std::time::Duration;
+use until::UntilExt;
 
 mod executor;
 
@@ -12,8 +14,11 @@ impl ThreadPool {
 	pub fn new_max(thread_count: usize) -> Self {
 		let (tx, rx) = flume::unbounded();
 
-		let threads = vec![];
-		std::thread::spawn(gen_executor(rx.clone()));
+		let handles = (0..thread_count)
+			.map(|_| std::thread::spawn(gen_executor(rx.clone())))
+			.collect::<Vec<_>>();
+
+		Self
 	}
 }
 
@@ -21,6 +26,9 @@ impl ThreadPool {
 fn gen_executor(rx: flume::Receiver<Task>) -> impl FnOnce() -> () {
 	|| loop {
 		// DO async tasks
-		for task in rx.try_iter().take(10) {} // Use until iter I just made later here because take sucks in that case
+		for task in rx.try_iter().do_for(Duration::from_millis(100)) {}
+		if rx.is_empty() {
+			std::thread::park_timeout(Duration::from_secs(30));
+		}
 	}
 }
