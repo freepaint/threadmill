8539096500eba8b5efe50ad18fb44670cbbf6a6d
König <post-rex@pm.me>
2020-11-30 14:14:24 +0100

    Deleted execution tracker

--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -2,18 +2,34 @@ use crate::task::{Task, TaskState};
 use std::time::Duration;
 use until::UntilExt;
 
-mod executor;
+pub struct ThreadPool {
+	scheduler: Scheduler,
+}
 
-pub struct ThreadPool {}
-
-#[derive(Clone)]
+#[derive(Clone, Default)]
 struct Scheduler {
 	/// Despite its name, priority only gets a single thread with the single goal of depleting this queue forever
-	priority: (flume::Sender<Box<dyn Task>>, flume::Receiver<Box<dyn Task>>),
+	priority: SchedulerQueue,
 	/// This queue processes all tasks single threaded with regular priority
-	regular: (flume::Sender<Box<dyn Task>>, flume::Receiver<Box<dyn Task>>),
+	regular: SchedulerQueue,
 	/// This queue processes all heavy work loads with n threads, n = num of cpus
-	work: (flume::Sender<Box<dyn Task>>, flume::Receiver<Box<dyn Task>>),
+	work: SchedulerQueue,
+}
+
+#[derive(Clone)]
+struct SchedulerQueue {
+	scheduler: flume::Sender<Box<dyn Task>>,
+	queue: flume::Receiver<Box<dyn Task>>,
+}
+
+impl Default for SchedulerQueue {
+	fn default() -> Self {
+		let (tx, rx) = flume::unbounded();
+		Self {
+			scheduler: tx,
+			queue: rx,
+		}
+	}
 }
 
 impl ThreadPool {
@@ -22,42 +38,37 @@ impl ThreadPool {
 	}
 
 	pub fn new_max(thread_count: usize) -> Self {
-		let scheduler = Scheduler {
-			priority: flume::unbounded(),
-			regular: flume::unbounded(),
-			work: flume::unbounded(),
-		};
+		let scheduler = Scheduler::default();
 
 		// Spawn work threads
 		let mut handles = (0..thread_count)
-			.map(|_| std::thread::spawn(gen_executor(scheduler.work.1.clone(), scheduler.work.0.clone())))
+			.map(|_| std::thread::spawn(gen_executor(scheduler.work.clone())))
 			.collect::<Vec<_>>();
 
 		// Spawn thread for regular queue
-		handles.push(std::thread::spawn(gen_executor(scheduler.regular.1.clone(), scheduler.regular.0.clone())));
+		handles.push(std::thread::spawn(gen_executor(scheduler.regular.clone())));
 
 		// Spawn thread for priority queue
-		handles.push(std::thread::spawn(gen_executor(scheduler.priority.1.clone(), scheduler.priority.0.clone())));
+		handles.push(std::thread::spawn(gen_executor(scheduler.priority.clone())));
 
-		Self {}
+		Self { scheduler }
 	}
 }
 
 // This got its own function for readability
-fn gen_executor(queue: flume::Receiver<Box<dyn Task>>, scheduler: flume::Sender<Box<dyn Task>>) -> impl FnOnce() {
+fn gen_executor(queue: SchedulerQueue) -> impl FnOnce() {
 	move || loop {
-		for mut task in queue.try_iter().do_for(Duration::from_millis(100)) {
+		for mut task in queue.queue.iter().do_for(Duration::from_millis(100)) {
 			match task.exec() {
 				// Reschedule task for next execution
-				TaskState::Reschedule => if let Err(_) = scheduler.send(task) {
-					// TODO: Log event
-					return; // Assuming queue is closed meaning the scheduler has shutdown
-				},
+				TaskState::Reschedule => {
+					if queue.scheduler.send(task).is_err() {
+						// TODO: Log event
+						return; // Assuming queue is closed meaning the scheduler has shutdown
+					}
+				}
 				TaskState::Done => {}
 			}
-		}
-		if queue.is_empty() {
-			std::thread::park_timeout(Duration::from_secs(30));
 		}
 	}
 }
